6fee50fcf39085c2cca8196767ce6144dac506af
promita41 <promita41>
2024-01-09 09:54:47 +0000

    commit

---
+++ (Clone) Getting started.py
@@ -0,0 +1,24 @@
+# Databricks notebook source
+print("Run Python")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Getting started with spark
+# MAGIC ## Databricks
+# MAGIC ### Delta lake
+# MAGIC ###### Magic command
+
+# COMMAND ----------
+
+# MAGIC %scala
+# MAGIC println("Run scala")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select "RUN SQL"
+
+# COMMAND ----------
+
+
